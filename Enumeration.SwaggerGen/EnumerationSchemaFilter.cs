9902e1538b0ffe8baadf5ccd1e5f5b3dcd693305
p-martinho <martinho.pj@gmail.com>
2022-01-02 16:16:46 +0000

    Add swagger support

---
+++ Enumeration.SwaggerGen/EnumerationSchemaFilter.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using Microsoft.OpenApi.Any;
+using Microsoft.OpenApi.Models;
+using PM.Enumeration;
+using PM.Enumeration.Extensions;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace Enumeration.SwaggerGen;
+
+/// <summary>
+/// Implements <see cref="ISchemaFilter"/> to transform <see cref="Enumeration{T}"/> schema to <see cref="Enum"/>.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public class EnumerationSchemaFilter : ISchemaFilter
+{
+    /// <inheritdoc />
+    public void Apply(OpenApiSchema schema, SchemaFilterContext context)
+    {
+        if (!context.Type.IsAssignableToEnumeration())
+        {
+            return;
+        }
+
+        var fields = context.Type
+            .GetFields(BindingFlags.Public | BindingFlags.Static | BindingFlags.DeclaredOnly)
+            .Select(f => f.Name)
+            .ToList();
+
+        schema.Enum = fields.ConvertAll<IOpenApiAny>(fieldName => new OpenApiString(fieldName));
+        schema.Type = "string";
+        schema.AllOf = null;
+        schema.Properties = null;
+    }
+}
