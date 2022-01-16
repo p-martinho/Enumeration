0d81da8bb63ea32b8cd62bad85402a77d731161e
p-martinho <martinho.pj@gmail.com>
2022-01-16 12:16:39 +0000

    Add sample for SwaggerGen

---
+++ samples/Enumeration.SwaggerGen.Sample/Enumerations/CommunicationType.cs
@@ -0,0 +1,22 @@
+﻿using PMart.Enumeration;
+using PMart.Enumeration.SwaggerGen;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace Enumeration.SwaggerGen.Sample.Enumerations;
+
+/// <summary>
+/// The communication type enumeration.
+/// </summary>
+[SwaggerSchemaFilter(typeof(EnumerationSchemaFilter))]
+public class CommunicationType : Enumeration<CommunicationType>
+{
+    public static readonly CommunicationType Email = new("Email");
+
+    public static readonly CommunicationType Sms = new("SMS");
+    
+    public static readonly CommunicationType PushNotification = new("PushNotification");
+
+    private CommunicationType(string value) : base(value)
+    {
+    }
+}
